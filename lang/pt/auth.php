1069c1567d58d3427ab0054745cd11f9ae7e2c5c
Raphael Alves <raphael.andrade98@outlook.com>
2024-05-19 21:31:34 -0300

    inital translation

---
+++ lang/pt/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Credenciais inválidas.',
+    'password' => 'Senha incorreta',
+    'throttle' => 'Muitas tentativa, por favor, tentar em breve em :seconds segundos.',
+    'register' => 'Cadastre-se',
+    'login' => 'Entrar',
+];
